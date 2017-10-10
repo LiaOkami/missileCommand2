0fd046f3ebb54c67bab1a5bc98ebe50b5f33f7c5
Pierrick Garcia <pierrick.garcia@hotmail.fr>
2017-10-10 13:45:06 +0200

    Add SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,22 @@
+#include <SFML/Graphics.hpp>
 #include "MissileCommand.hh"
 
-int	main()
+int     main()
 {
-  MissileCommand	game;
+  MissileCommand        game;
+  sf::RenderWindow      window(sf::VideoMode(200, 200), "Missile Command");
+  sf::Event             event;
 
+  while (window.isOpen())
+  {
+    window.clear();
+    window.display();
+    while (window.pollEvent(event))
+      {
+        if (event.type == sf::Event::Closed)
+          window.close();
+    }
+  }
   game.launch();
   return (0);
 }
