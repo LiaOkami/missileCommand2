941512902ecb986f97ce4d504588372f00aedeb9
Okami <pierrick.garcia@hotmail.fr>
2017-10-14 10:50:39 +0200

    Update MissileCommand documentation

--- include/MissileCommand.hh
+++ include/MissileCommand.hh
@@ -1,22 +1,21 @@
-/**
- * \file MissileCommand.hh
- * \brief Classe MissileCommand
- * \author Pierrick Garcia
- *
- * Classe contenant tout le jeu MissileCommand
+/** \file MissileCommand.hh
+ *  \brief Classe MissileCommand
  */
 
 #ifndef _MISSILECOMMAND_HH_
 # define _MISSILECOMMAND_HH_
 
+/** \brief Classe contenant tout le jeu Missile Command
+ *  \author Pierrick Garcia
+ */
 class MissileCommand
 {
 public:
+  /** \brief Constructeur par défaut
+   */
   MissileCommand();
 
-  /**
-   * \fn void launch()
-   * \brief Lance le jeu
+  /** \brief Lance le jeu
    *
    * Appelé au début du programme.
    * L'intégralité du jeu est exécutée dans cette méthode.
