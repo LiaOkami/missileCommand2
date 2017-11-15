99f4ad098f17a4b5f3e3e8345e44412ecb2d22be
Okami <pierrick.garcia@hotmail.fr>
2017-11-15 13:10:09 +0100

    Change assets path

--- src/main.cpp
+++ src/main.cpp
@@ -28,113 +28,113 @@ int     main()
 
     // Chargement de l'image
     sf::Texture missile;
-    if(!missile.loadFromFile("Images/missile_01.png", sf::IntRect(0,50,253,78))) //[1] Chemin de l'image, [2] Cadre de l'image
+    if(!missile.loadFromFile("assets/Missile.png", sf::IntRect(0,50,253,78))) //[1] Chemin de l'image, [2] Cadre de l'image
     {
-        cout << "Erreur chargement texture (missile_01.png)" << endl;
+	cout << "Erreur chargement texture (missile_01.png)" << endl;
     }
 
 
     while (window.isOpen())
     {
-        /* Instructions d'affichage */
-        window.clear();
-        window.draw(pointeurSouris);
+	/* Instructions d'affichage */
+	window.clear();
+	window.draw(pointeurSouris);
 
-        int tabMSize = tabMissiles.size();
-        for (int cpt = 0; cpt < tabMSize; cpt++)
-        {
-            window.draw(tabMissiles[cpt]);
-        }
+	int tabMSize = tabMissiles.size();
+	for (int cpt = 0; cpt < tabMSize; cpt++)
+	{
+	    window.draw(tabMissiles[cpt]);
+	}
 
-        window.display(); //Affichage
+	window.display(); //Affichage
 
-        /* Boucle d'évènements */
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed) //
-                window.close();
-            if (event.type == sf::Event::KeyPressed)
-                window.close();
-
-
-            /** \brief met à jour la position du pointeur avec la position du curseur de la souris */
-            sf::Vector2f positionSouris(sf::Mouse::getPosition(window));
-            if (event.MouseMoved)
-            {
-                pointeurSouris.setPosition(positionSouris);
-            }
+	/* Boucle d'évènements */
+	while (window.pollEvent(event))
+	{
+	    if (event.type == sf::Event::Closed) //
+		window.close();
+	    if (event.type == sf::Event::KeyPressed)
+		window.close();
 
 
-            /** \brief Si l'utilisateur clique sur le missile, il est supprimé */
-
-            if(event.type == sf::Event::MouseButtonPressed)
-            {
-                if (event.mouseButton.button == sf::Mouse::Left && tabMissiles.size()>0)
-                {
-                    tabMSize = tabMissiles.size();
-                    sf::Vector2f positionSouris2(sf::Mouse::getPosition(window));
-                    for(int cpt1 = 0; cpt1 < tabMSize; cpt1++) // vérification pour chaque missile
-                    {
-                        if(tabMissiles[cpt1].getGlobalBounds().contains(positionSouris2)) // Si la souris est sur le sprite, décalage et suppression
-                        {
-                            cout << "Missile cliqué. ";
-                            for(int cpt2 = cpt1; cpt2 < tabMSize-1; cpt2++)
-                            {
-                                tabMissiles[cpt2] = tabMissiles[cpt2+1];
-                            }
-                            cout << "Missile supprimé." << endl;
-                            tabMissiles.pop_back();
-                        }
-                    }
-                }
-            }
-        }
-
-        /* Mise à jour des éléments */
-
-        /** \brief fait bouger les missiles*/
-        if ( chrono.getElapsedTime() > dureeMax)
-        {
-            tabMSize = tabMissiles.size();
-            for (int cpt = 0; cpt < tabMSize; cpt++)
-            {
-                tabMissiles[cpt].move(0,7);
-            }
-            chrono.restart();
-        }
+	    /** \brief met à jour la position du pointeur avec la position du curseur de la souris */
+	    sf::Vector2f positionSouris(sf::Mouse::getPosition(window));
+	    if (event.MouseMoved)
+	    {
+		pointeurSouris.setPosition(positionSouris);
+	    }
 
 
-        /** \brief ajoute un missile au tableau de missiles lorsque la duée d'apparition est écoulée */
-        if (apparition.getElapsedTime() > dureeApparition)
-        {
-            sf::Sprite sprite_missile;
-            int x = (rand()%(tailleF.x-78/2)) +78/2;
+	    /** \brief Si l'utilisateur clique sur le missile, il est supprimé */
 
-            sprite_missile.setTexture(missile);
-            sprite_missile.scale(0.5,0.5);
-            sprite_missile.setRotation(90.0);
-            sprite_missile.setPosition(x,0);
-            tabMissiles.push_back(sprite_missile);
-            apparition.restart();
-            cout << "Création d'un missile" << endl;
-        }
+	    if(event.type == sf::Event::MouseButtonPressed)
+	    {
+		if (event.mouseButton.button == sf::Mouse::Left && tabMissiles.size()>0)
+		{
+		    tabMSize = tabMissiles.size();
+		    sf::Vector2f positionSouris2(sf::Mouse::getPosition(window));
+		    for(int cpt1 = 0; cpt1 < tabMSize; cpt1++) // vérification pour chaque missile
+		    {
+			if(tabMissiles[cpt1].getGlobalBounds().contains(positionSouris2)) // Si la souris est sur le sprite, décalage et suppression
+			{
+			    cout << "Missile cliqué. ";
+			    for(int cpt2 = cpt1; cpt2 < tabMSize-1; cpt2++)
+			    {
+				tabMissiles[cpt2] = tabMissiles[cpt2+1];
+			    }
+			    cout << "Missile supprimé." << endl;
+			    tabMissiles.pop_back();
+			}
+		    }
+		}
+	    }
+	}
+
+	/* Mise à jour des éléments */
+
+	/** \brief fait bouger les missiles*/
+	if ( chrono.getElapsedTime() > dureeMax)
+	{
+	    tabMSize = tabMissiles.size();
+	    for (int cpt = 0; cpt < tabMSize; cpt++)
+	    {
+		tabMissiles[cpt].move(0,7);
+	    }
+	    chrono.restart();
+	}
 
 
-        /** \brief supprime les missiles en dehors de la fenêtre */
-        tabMSize = tabMissiles.size();
-        for(int cpt1 = 0; cpt1 < tabMSize; cpt1++)
-        {
-            if(tabMissiles[cpt1].getPosition().y > tailleF.y-50)
-            {
-                cout << "Missile sorti ( " << tabMissiles[cpt1].getPosition().y << " > " << tailleF.y-50 << " ). ";
-                for(int cpt2 = cpt1; cpt2 < tabMSize-1; cpt2++)
-                {
-                    tabMissiles[cpt2] = tabMissiles[cpt2+1];
-                }
-                cout << "Missile supprimé." << endl;
-                tabMissiles.pop_back();
-            }
-        }
+	/** \brief ajoute un missile au tableau de missiles lorsque la duée d'apparition est écoulée */
+	if (apparition.getElapsedTime() > dureeApparition)
+	{
+	    sf::Sprite sprite_missile;
+	    int x = (rand()%(tailleF.x-78/2)) +78/2;
+
+	    sprite_missile.setTexture(missile);
+	    sprite_missile.scale(0.5,0.5);
+	    sprite_missile.setRotation(90.0);
+	    sprite_missile.setPosition(x,0);
+	    tabMissiles.push_back(sprite_missile);
+	    apparition.restart();
+	    cout << "Création d'un missile" << endl;
+	}
+
+
+	/** \brief supprime les missiles en dehors de la fenêtre */
+	tabMSize = tabMissiles.size();
+	for(int cpt1 = 0; cpt1 < tabMSize; cpt1++)
+	{
+	    if(tabMissiles[cpt1].getPosition().y > tailleF.y-50)
+	    {
+		cout << "Missile sorti ( " << tabMissiles[cpt1].getPosition().y << " > " << tailleF.y-50 << " ). ";
+		for(int cpt2 = cpt1; cpt2 < tabMSize-1; cpt2++)
+		{
+		    tabMissiles[cpt2] = tabMissiles[cpt2+1];
+		}
+		cout << "Missile supprimé." << endl;
+		tabMissiles.pop_back();
+	    }
+	}
 
     }
     game.launch();
