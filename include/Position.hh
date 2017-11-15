356c25332fa0ab5d756b346e7904e4304a032371
Okami <pierrick.garcia@hotmail.fr>
2017-11-15 16:46:40 +0100

    Move Poisition to its own header to prevent circular inclusion

---
+++ include/Position.hh
@@ -0,0 +1,19 @@
+/** \file Position.hh
+ *  \brief Classe Position
+ */
+
+#ifndef _POSITION_HH_
+# define _POSITION_HH_
+
+# include <stdint.h>
+
+/** \brief Structure représentant la Position d'un Object
+ *  \author Pierrick Garcia
+ */
+struct Position
+{
+  uint16_t	x;
+  uint16_t	y;
+};
+
+#endif
