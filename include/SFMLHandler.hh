84d3f9d6e049a2de0ede28332c2c82442ac5c5d7
Okami <pierrick.garcia@hotmail.fr>
2017-11-15 13:32:07 +0100

    Add SFMLHander header file

---
+++ include/SFMLHandler.hh
@@ -0,0 +1,29 @@
+/** \file SFMLHandler.hh
+ *  \brief Classe SFMLHandler
+ */
+
+#ifndef _SFMLHANDLER_HH_
+# define _SFMLHANDLER_HH_
+
+/** \brief Classe encapsulant la bibliothèque SFML
+ *  \author Etienne Berrié
+ */
+class SFMLHhandler
+{
+public:
+  /** \brief Constructeur par défaut
+   */
+  SFMLHandler();
+
+  /** \brief Destructeur par défaut
+   */
+  ~SFMLHandler();
+
+  void	clearWindow();
+
+  void	addAsset(const std::string & key, const std::string & filename);
+
+  void	draw(const std::string & key, int x, int y);
+};
+
+#endif
