b4f0709b8db1d889e65ca7e4168c0152d2061162
EtienneBrx <etienne.berrie@gmail.com>
2017-11-21 13:37:59 +0100

    Ajout de src/SFML.cpp

---
+++ src/SFMLHandler.cpp
@@ -0,0 +1,17 @@
+/** \file SFMLHandler.cpp
+ *  \brief Classe SFMLHandler
+ */
+
+#include "SFMLHandler.hh"
+
+using namespace std;
+
+SFMLHandler::SFMLHandler(int width, int height, string title)
+{
+    sf::RenderWindow window(sf::VideoMode(width, height), title)
+}
+
+void SFMLHandler::clearWindow()
+{
+
+}
