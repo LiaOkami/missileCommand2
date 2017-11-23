2e2392fec79c5ed9202eef360f600696007187f7
Palex-Iut <pierre-alexandre.jozeau@u-psud.fr>
2017-11-23 11:19:17 +0100

    Change Position to float
    Signed-off-by: palex-k <palex-k@palex-k-TravelMate-5742>

--- include/Object.hh
+++ include/Object.hh
@@ -6,14 +6,24 @@
 # define _OBJECT_HH_
 
 # include "SFMLHandler.hh"
+#include "position.hh"
+using namespace std;
 
 /** \brief Classe parente de tous les objets affichables
  *  \author Pierre-Alexandre Jozeau
  */
 class Object
 {
+private:
+
+Position _pos;
+string _key;
+
+
+
 public:
   /** \brief Constructeur par défaut
+
    */
   Object(float x, float y, const std::string &key);
 
