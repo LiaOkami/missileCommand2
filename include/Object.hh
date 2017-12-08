bfb3099deeda64c42ad2d330c71487a468eba520
Palex-Iut <pierre-alexandre.jozeau@u-psud.fr>
2017-12-08 12:38:16 +0100

    Update Doxygen documentation on Object.hh, debug and complete Object.hh & Object.cpp

--- include/Object.hh
+++ include/Object.hh
@@ -10,9 +10,10 @@
 
 using namespace std;
 
-/** \brief Classe parente de tous les objets affichables
- *  \author Pierre-Alexandre Jozeau
- */
+    /** \brief Classe parente de tous les objets affichables
+    *  \author Pierre-Alexandre Jozeau
+    */
+
 class Object
 {
 private:
@@ -21,22 +22,38 @@ private:
     string _key;
 
 public:
-    /** \brief Constructeur par défaut
+ /*    /** \brief Constructeur par défaut
      *
      *  \param float x abcisse de l'objet
      *  \param float y ordonnée de l'objet
      *  \param const string key
      *
      *  Construction de l'objet, ayant pour attributs une position, abcisse et ordonnée, et une key
-     *
      */
-    Object(const Position &pos, const string &key);
-    ///doxy draw
-    void	draw(SFMLHandler& handler) const;
+    //Object(const Position &, const string &);*/
 
-    ///doxy update
 
-    virtual void	update(); //renvoie à l'update de niveau inferieur si update different
+    /** \brief affichage de l'objet
+    *  \param handler qui récupère la clé et affiche l'objet correspondant
+    *
+    *  Construction de l'objet, ayant pour attributs une position, abcisse et ordonnée, et une key
+    */
+    void draw(SFMLHandler& handler) const;
+
+
+    void setPos(const Position &pos);/**< \brief _Position setter */
+
+
+    void setKey(const string &key);/**< \brief _Key setter */
+
+
+    /** \brief fonction update
+    *
+    *  \param update pour l'affichage
+    *
+    *  Fonction de mise à jour de l'affichage d'Object, dépend de l'update des classes héritières (virtual)
+    */
+    virtual void update(); //renvoie à l'update de niveau inferieur si update different
 };
 
 #endif
--- src/Object.cpp
+++ src/Object.cpp
@@ -2,10 +2,20 @@
 
 using namespace std;
 
-Object::Object(const Position &pos, const string &key)
+/*Object::Object(const Position &pos, const string &key)
 {
     _key = key;
     _pos = pos;
+}*/
+
+void Object::setPos(const Position &pos)
+{
+    _pos = pos;
+}
+
+void Object::setKey(const string &key)
+{
+    _key = key;
 }
 
 void Object::draw(SFMLHandler& handler) const
