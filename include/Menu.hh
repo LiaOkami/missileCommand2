c6b3f24b898982b83f582b89f644296820c00270
azerteriche <romain.touzelin.iut@gmail.com>
2018-01-12 16:44:36 +0100

    Add loadAsset for class Menu

--- include/Menu.hh
+++ include/Menu.hh
@@ -5,7 +5,7 @@
 class Menu
 {
     public:
-        Menu();
+        Menu(SFMLHandler &window);
     private:
 };
 
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,6 +1,9 @@
 #include "Menu.hh"
 
-Menu::Menu()
+Menu::Menu(SFMLHandler &window)
 {
-    //ctor
+    window.loadAsset("BUTTON_START","assets/Button_Start.png");
+    window.loadAsset("BUTTON_SCORE","assets/Button_Score.png");
+    window.loadAsset("BUTTON_EXIT","assets/Button_Exit.png");
+    window.loadAsset("BACKGROUND", "assets/MissileCommand_Background.jpg");
 }
